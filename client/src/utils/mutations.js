485828c4a5852bc04234b4bd05485aba53a280ce
MariaFernandaMarroquin <MariaFernanda@MacBook-Pro-de-Martha.local>
2023-08-18 14:10:24 -0600

    addUser mutation correct in client side

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -13,7 +13,7 @@ mutation login($email: String!, $password: String!) {
 `;
 
 export const ADD_USER = gql`
-mutation addUser($username: String!, $email: String! $password: String!) {
+mutation addUser($username: String!, $email: String!, $password: String!) {
     addUser(username: $username, email: $email, password: $password) {
         token
         user {
