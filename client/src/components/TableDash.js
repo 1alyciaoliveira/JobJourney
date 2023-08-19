05b1135882976c22fd4113f89f1044e281fc3173
Maelikah <eva.longoria@gmail.com>
2023-08-19 14:22:02 -0600

    added fetch data table functionality

--- client/src/components/TableDash.js
+++ client/src/components/TableDash.js
@@ -1,70 +1,150 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {Button} from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { QUERY_ME } from '../utils/queries';
+import { REMOVE_APPLICATION } from '../utils/mutations';
+import { useMutation, useQuery } from '@apollo/client';
+import Auth from '../utils/auth';
 
 function TableDash() {
 
-// aqui hay que crear la funcion que hace que los modales salgan a preguntar si quiere borrarse la fila de la vacante
-function deleteJob () {
-    console.log("hola");
-}
+  const [showModal, setShowModal] = useState(false);
+  const [selectedJobIdToDelete, setSelectedJobIdToDelete] = useState(null);
+  const [showDeleteModal, setShowDeleteModal] = useState(false);
+  const [removeJobApplication] = useMutation(REMOVE_APPLICATION);
+
+  const { loading, error, data } = useQuery(QUERY_ME);
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error: {error.message}</p>;
+
+  const user = data.me;
+  const userJobs = user.jobsApplied;
+
+  const confirmDelete = async (_id) => {
+    const token = Auth.loggedIn() ? Auth.getToken() : null;
+
+    if (!token) {
+      return false;
+    }
+
+    try {
+      await removeJobApplication({
+        variables: { _id },
+      });
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  const openModal = (job) => {
+    setSelectedJobIdToDelete(job._id);
+    setShowModal(true);
+  };
+
+  const handleDelete = (e) => {
+    e.stopPropagation();
+    setShowDeleteModal(true);
+    confirmDelete(selectedJobIdToDelete);
+  };
+
+  // Create a sorted copy of userJobs array
+  const sortedUserJobs = [...userJobs].sort((jobA, jobB) =>
+    jobA.status.localeCompare(jobB.status)
+  );
 
   return (
-  <div>  
+    <div>
+      <div className="container table-responsive">
+        <h2>Job Applications</h2>
+        <br/>
+        <table className="table table-striped">
+          <thead className="thead-dark text-center">
+            <tr>
+              <th>Company</th>
+              <th>URL</th>
+              <th>Position</th>
+              <th>Salary</th>
+              <th>Notes</th>
+              <th>Status</th>
+              <th>Reminder Date</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            {userJobs.map((job, index) => (
+              <tr key={job._id} className="text-center">
+                <td>{job.company}</td>
+                <td>{job.url}</td>
+                <td>{job.jobPosition}</td>
+                <td>{job.salary}</td>
+                <td>{job.comments}</td>
+                <td>{job.status}</td>
+                <td>{job.reminderDate}</td>
+                <td>
+                  <Button
+                      variant="dark btn-sm mr-2"
+                      onClick={() => openModal(job)}
+                    >
+                      Edit
+                  </Button>
+                  <Button
+                    variant="danger btn-sm"
+                    onClick={() => openModal(job)}
+                  >
+                    Delete
+                  </Button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
 
-{/* falta conectar que cada que agrega el usuario alguna application vayan apareciendo aqui, necesito saber como traerme esa informacion a cada row que se vaya creando */}
-    <div class="container table-responsive">
-      <h2>Job Applications</h2>
-      <p>Check the status of your current applications:</p>            
-      <table class="table">
-        <thead class="text-center">
-          <tr>
-            <th>Company</th>
-            <th>Position</th>
-            <th>Notes</th>
-            <th>Status</th>
-            <th>......</th>
-          </tr>
-        </thead>
-        <tbody>
-            {/* aqui debe entrar una funcion que me traiga la informacion de las vacantes que el usuario haya puesto, de la fila 41 a la 50 se repetira las veces que haya vacantes */}
-          <tr>
-            <td>1 name</td>
-            <td>job name</td>
-            <td>notes written by me</td>
-            <td>On Interview</td>
-            <td><button class="btn-sm btn-warning">Edit</button>
-                <Button variant="danger btn-sm" onClick={deleteJob}>Delete
-                </Button>
-            </td>
-          </tr>
-          
-        </tbody>
-      </table>
-    </div>
-    
-
-{/* <!-- Delete Modal ** falta que cuando se cargue la pagina otra vez ese que estaba delete ya NO aparezca--> */}
-<div id="delModal" class="modal fade" role="dialog">
-    <div class="modal-dialog">
-      <div class="modal-content">
-        <div class="modal-header">
-          <h4 class="modal-title">Delete Job Application</h4>
-          <button type="button" class="close" data-dismiss="modal">&times;</button>
-        </div>
-        <div class="modal-body">
-          <p>Are you sure you want to delete this job application?</p>
-        </div>
-        <div class="modal-footer">
-          <button type="button" class="btn btn-danger" id="confirmDel">Yes</button>
-          <button type="button" class="btn btn-secondary" id="cancelDel" data-dismiss="modal">No</button>
+      <div
+        id="delModal"
+        className="modal fade"
+        role="dialog"
+      >
+        {/* Delete Modal */}
+        <div className="modal-dialog">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h4 className="modal-title">Delete Job Application</h4>
+              <button
+                type="button"
+                className="close"
+                data-dismiss="modal"
+              >
+                &times;
+              </button>
+            </div>
+            <div className="modal-body">
+              <p>
+                Are you sure you want to delete this job application?
+              </p>
+            </div>
+            <div className="modal-footer">
+              <button
+                type="button"
+                className="btn btn-danger"
+                id="confirmDel"
+              >
+                Yes
+              </button>
+              <button
+                type="button"
+                className="btn btn-secondary"
+                id="cancelDel"
+                data-dismiss="modal"
+              >
+                No
+              </button>
+            </div>
+          </div>
         </div>
       </div>
     </div>
-  </div>
-  
-</div>
-
-)
+  );
 }
 
 export default TableDash;
