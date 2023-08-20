4358a433b5b3d8285af9467336c7a7decfea5209
Maelikah <eva.longoria@gmail.com>
2023-08-20 13:08:53 -0600

    fixed auth login issue

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 
 import {
   ApolloClient,
@@ -12,7 +12,7 @@ import { setContext } from "@apollo/client/link/context";
 import LoginForm from './components/LoginForm';
 import SignForm from './components/SignForm';
 import Dashboard from './components/Dashboard';
-import TableDash from './components/TableDash';
+
 
 // Construct our main GraphQL API endpoint
 const httpLink = createHttpLink({
@@ -55,7 +55,6 @@ function App() {
             <SignForm onSwitchForm={handleFormSwitch} />
           )} />
           <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/tabledash" element={<TableDash />} />
         </Routes>
       </Router>
 
