9585698271d25f2d9c87adf429bdf6acfe4035dc
Blanca Nelly Fabela Guardado <nelly.fabela@Nelly-MacBook-Pro.local>
2023-08-19 17:52:29 -0700

    deleting the div of the dashboard inside return and mak ing the logo and JobJourney one as a button in order to be clickable and reload the page

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -6,17 +6,31 @@ import logo from '../image/logo.png';
 function Header(props) {
 
     const navigate = useNavigate();
+    
+    // constant for reloading the dashbaord page when clicking on the logo + JobJourney
+    const handleReloadDashboard = () => {
+        window.location.reload(); 
+    }
+
+    const handleLogout = () => {
+        // Clear token and redirect to login page
+        localStorage.removeItem('id_token');
+        navigate('/'); // Redirect to the login page
+    };
 
     return (
         <div className="container-fluid bg-light text-black align-middle">
             <div className="row py-3">
                 <div className="col-12 text-center d-flex align-items-center justify-content-between">
                     <div className="d-flex align-items-center">
-                        <img src={logo} alt="JobJourney Logo" className="logo-image" />
-                        <h1 className="mb-4 mt-4 align-center">JobJourney</h1>
+                        <button className="btn" onClick={handleReloadDashboard}>
+                            <div className="d-flex align-items-center">
+                                <img src={logo} alt="JobJourney Logo" className="logo-image" />
+                                <h1 className="mb-4 mt-4 align-center">JobJourney</h1>
+                            </div>
+                        </button>
                     </div>
                 <div className="d-flex align-items-center">
-                    <button className="btn" onClick={() => props.setShowDashboard(true)}><span><i class="fas fa-home"></i></span>   Dashboard</button>
                     <UserProfile />
                     <button className="btn"  onClick={handleLogout}><span><i class="fas fa-sign-out-alt"></i></span>   Logout</button>
                 </div>
