df2288efb5306dc19045f73628b2093e395ac7dd
MariaFernandaMarroquin <fer.marroquin2@gmail.com>
2023-08-20 22:04:24 -0600

    change in server.js for handle deployment

--- server/server.js
+++ server/server.js
@@ -23,7 +23,7 @@ if (process.env.NODE_ENV === 'production') {
 }
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build'));
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
 
 
