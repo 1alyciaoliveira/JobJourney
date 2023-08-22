94475a006b8eb86d200a49c41c56d3c9557ff98f
Alycia Oliveira <aly.oliveira@spectrumeffect.com>
2023-08-21 19:58:41 -0600

    testing with *

--- server/server.js
+++ server/server.js
@@ -28,11 +28,11 @@ app.use(express.json());
 app.use(cors(corsOption));
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/dist')));
+  app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
 
 
