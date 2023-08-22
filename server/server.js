556512eb28edca72a70af4df28cf78ceb518f45a
Alycia Oliveira <aly.oliveira@spectrumeffect.com>
2023-08-21 19:56:32 -0600

    testing with *

--- server/server.js
+++ server/server.js
@@ -31,7 +31,7 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/dist')));
 }
 
-app.get('/', (req, res) => {
+app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/dist/index.html'));
 });
 
