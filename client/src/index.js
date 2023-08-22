ae6eba829111e01cfc97e20e254ca82d94b6464c
MariaFernandaMarroquin <fer.marroquin2@gmail.com>
2023-08-21 21:11:51 -0600

    configuration for PWA, service worker not working

--- client/src/index.js
+++ client/src/index.js
@@ -4,6 +4,8 @@ import './index.css';
 import './bootstrap.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+import registerServiceWorker from "./service-worker";
 
 ReactDOM.render(
   <React.StrictMode>
@@ -12,7 +14,19 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+// serviceWorkerRegistration.register();
+registerServiceWorker();
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+
+
+
+
+
